8321234629614fc8724774a843a1294bb42eadb5
walele993 <64219379+walele993@users.noreply.github.com>
2025-03-17 18:47:59 +0100

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.json',
     }],
-  },
+  }, 
   moduleNameMapper: {
     '^axios$': '<rootDir>/__mocks__/axios.ts',
     '^node-fetch$': '<rootDir>/__mocks__/node-fetch.ts',
